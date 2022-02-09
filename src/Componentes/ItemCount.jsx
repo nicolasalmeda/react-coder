305356aa7bb6e13a9f0c6522ca7b58754a39b069
Nicolas Ala <nicoalmeda@gmail.com>
2022-02-09 19:18:53 -0300

    cuarta entrega

---
+++ src/Componentes/ItemCount.jsx
@@ -0,0 +1,33 @@
+import styles from "./ItemCount.module.css";
+import { useState } from "react";
+
+export default function ItemCount({ stock, initial, onAdd }) {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > initial) setCount(count - 1);
+  };
+
+  return (
+    <div className={styles.itemCountContainer}>
+      <div className={styles.childrenContainer}>
+        <button onClick={increment} className={styles.button}>
+          +
+        </button>
+        <div>{count}</div>
+        <button onClick={decrement} className={styles.button}>
+          -
+        </button>
+        <div>
+          <button onClick={onAdd}>Agregar al carrito</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Componentes/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer.jsx
@@ -1,3 +1,10 @@
+import ItemCount from "./ItemCount";
+
 export default function ItemListContainer({ greeting }) {
-  return <h1>{greeting}</h1>;
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <ItemCount initial="1" stock="50" />
+    </div>
+  );
 }
