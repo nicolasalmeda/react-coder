23b39a6473440d436b3ad7b884e69d3a4458e099
Nicolas Ala <nicoalmeda@gmail.com>
2022-03-01 22:37:38 -0300

    correcciones de cardCart

--- src/Componentes/CardCart.jsx
+++ src/Componentes/CardCart.jsx
@@ -1,10 +1,15 @@
-export default function CardCart({ items }, finalPrice, removeItem) {
+import { useContext } from "react";
+import CartContext from "../context/CartContext";
+
+export default function CardCart({ items }) {
+  const { removeItem } = useContext(CartContext);
+
   return (
     <div>
       <h3>{items.name}</h3>
       <button onClick={() => removeItem(items.id)}>Remove</button>
       <img src={items.img} alt="img" />
-      <p> Price: {finalPrice} </p>
+      <p> Price: {items.price} </p>
     </div>
   );
 }
--- src/Componentes/Cart.jsx
+++ src/Componentes/Cart.jsx
@@ -1,37 +1,35 @@
 import { useContext } from "react";
 import CartContext from "../context/CartContext";
-// import CardCart from "./CardCart";
+import CardCart from "./CardCart";
 
 export default function Cart() {
-  const { cart, removeItem, clear } = useContext(CartContext);
+  const { cart, clear } = useContext(CartContext);
+
   console.log("cart en Cart:", cart);
-  let sumador = 0;
+  let total = 0;
   return (
     <div>
       <ul>
         {cart.map((items) => {
-          const finalPrice = items.price * items.totalItem;
+          const subtotal = items.price * items.totalItem;
 
-          sumador = sumador + finalPrice;
+          total = total + subtotal;
 
           return (
-            <div key={items.id}>
-              <h3>{items.name}</h3>
-              <button onClick={() => removeItem(items.id)}>Remove</button>
-              <img src={items.img} alt="img" />
-              <p> Price: {finalPrice} </p>
+            // <div key={items.id}>
+            //   <h3>{items.name}</h3>
+            //   <button onClick={() => removeItem(items.id)}>Remove</button>
+            //   <img src={items.img} alt="img" />
+            //   <p> Price: {finalPrice} </p>
+            // </div>
+            <div>
+              <CardCart key={items.id} items={items} />
+              <p>SubTotal: {subtotal} </p>
             </div>
-
-            //   <CardCart
-            //     key={items.id}
-            //     finalPrice={finalPrice}
-            //     items={items}
-            //     removeItem={removeItem}
-            //   />
           );
         })}
       </ul>
-      <p>Precio Total:{sumador}</p>
+      <p>Precio Total:{total}</p>
       <button onClick={() => clear()}>Clear</button>
     </div>
   );
